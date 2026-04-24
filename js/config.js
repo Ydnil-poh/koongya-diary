3f7d69cfcbaf931160365e851b89cc3ad9b58279
Ydnil-poh <warpart@naver.com>
2026-04-24 10:28:41 +0900

    Delete js/config.js

--- js/config.js
+++
@@ -1,5 +0,0 @@
-export const CONFIG = {
-    SUPABASE_URL: 'https://nquysdhnktaburanulwt.supabase.co',
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xdXlzZGhua3RhYnVyYW51bHd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MTM2OTIsImV4cCI6MjA5MTM4OTY5Mn0.1N6eMo18iQtPzc5pzgebRl9Eg2yhuLAXp9_ILeHRpUY',
-    GEMINI_API_KEY: 'AIzaSyAogvyVfruuZ70ANiH1cQo70ldLWctylrA'
-};
